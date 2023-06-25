3826d39e6eda435aafdc6e9dbf8cd7155a16707f
Allen <allenvs@mail.ru>
2023-06-25 21:51:25 +0600

    lesson 7: Exercise: Equivalent Binary Trees
    * Walk does the magic in the background
    * Same read the x and y from channels and compares them

---
+++ gotour/concurrency/7_exercise_equivalent_binary_trees/go.mod
@@ -0,0 +1,5 @@
+module allen/other/go-tour/gotour/concurrency/7_exercise_equivalent_binary_trees
+
+go 1.20
+
+require golang.org/x/tour v0.1.0
---
+++ gotour/concurrency/7_exercise_equivalent_binary_trees/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	if t != nil {
+		Walk(t.Left, ch)
+		ch <- t.Value
+		Walk(t.Right, ch)
+	}
+}
+
+// Walker calls Walk and closes ch
+func Walker(t *tree.Tree, ch chan int) {
+	Walk(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walker(t1, ch1)
+	go Walker(t2, ch2)
+
+	for i := 0; i < 10; i++ {
+		x, y := <-ch1, <-ch2
+		fmt.Printf("(%v %v) ", x, y)
+		if x != y {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walker(tree.New(1), ch)
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
