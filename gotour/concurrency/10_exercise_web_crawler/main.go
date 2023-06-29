7f3460d77ab34c5e2e603d270fa6698121c2da8d
Allen <allenvs@mail.ru>
2023-06-29 13:44:57 +0600

    10 Exercise: Web Crawler
    * Fetcher interface
    * Fetch returns body and URls from the page

---
+++ gotour/concurrency/10_exercise_web_crawler/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+
