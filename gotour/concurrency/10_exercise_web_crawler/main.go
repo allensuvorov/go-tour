a9e13d47f5a6b8c319c74488596440e6383ce1b3
Allen <allenvs@mail.ru>
2023-07-01 20:47:04 +0600

    lesson 10/11 Exercise: Web Crawler
    * fetcher is a populated fakeFetcher

--- gotour/concurrency/10_exercise_web_crawler/main.go
+++ gotour/concurrency/10_exercise_web_crawler/main.go
@@ -49,3 +49,37 @@ func (f fakeFetcher) Fetch(url string) (string, []string, error) {
 	}
 	return "", nil, fmt.Errorf("non found: %s", url)
 }
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFatcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
